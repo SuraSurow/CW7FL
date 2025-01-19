06595925a6f3b88c5144ea03f7cc8af672e6f831
0xKitt <krzysztof.kalisz@tuta.io>
2025-01-19 16:02:47 +0100

    partialy working addition

--- CW7FL/Calculator.cpp
+++ CW7FL/Calculator.cpp
@@ -1,12 +1,12 @@
 #include "Calculator.h"
-
 #include <algorithm>
 #include <list>
 #include <stdexcept>
 #include <unordered_set>
 
 Calculator::Calculator(std::string init) : init(std::move(init)) {
-
+    parser();
+    calculate("");
 }
 
 void Calculator::parser() {
@@ -17,23 +17,29 @@ void Calculator::parser() {
     if (base_system == "BIN") {num_sym = bin_symb;}
     std::unordered_set<std::string> num_sys_set(num_sym.begin(), num_sym.end());
     std::unordered_set<std::string> oper_set(operation_symb.begin(), operation_symb.end());
-    for (char c : this->init) {
+
+    for (int i = 0; i < init.size(); i++) {
         std::string temp_number;
-        if (num_sys_set.contains(&c)) {
-            temp_number += c;
-            continue;
+        std::string str_iter(1, init[i]);
+        if (oper_set.contains(str_iter)) {
+            this->tokenize.push_back(str_iter);
         }
-        if (oper_set.contains(&c)) {
-            this->tokenize.push_back(temp_number);
-            temp_number = "";
-            temp_number += c;
-            this->tokenize.push_back(temp_number);
-            temp_number = "";
-            continue;
+        int j = i;
+        while (num_sys_set.contains(str_iter) && j<init.size()) {
+            str_iter = std::string(1, init[j]);
+            temp_number += str_iter;
+            j++;
+            str_iter = std::string(1, init[j]);
         }
+        if (!temp_number.empty()){
+            this->tokenize.push_back(temp_number);
+            i=j-1;
+        }
+        temp_number.clear();
     }
+
     for (auto it : this->tokenize) {
-        if (num_sys_set.contains(it)) {
+        if (oper_set.contains(it)) {
             mixed_tokens.emplace_back(it);
         }else {
             if (base_system == "HEX") {mixed_tokens.emplace_back(std::stoi(it, nullptr, 16));}
@@ -46,6 +52,7 @@ void Calculator::parser() {
 }
 
 void Calculator::calculate(std::string math_operator) {
+    std::string m_op;
     // Pusta lista , lub tylko operator
     if (mixed_tokens.empty() ||
        (mixed_tokens.size() == 1 && std::holds_alternative<std::string>(mixed_tokens.front()))) {
@@ -59,7 +66,10 @@ void Calculator::calculate(std::string math_operator) {
         return_result();
         return;
     }
-
+    if (std::holds_alternative<std::string>(mixed_tokens.front())) {
+        m_op = std::get<std::string>(mixed_tokens.front());
+        mixed_tokens.pop_front();
+    }
     // stos działań
     while (!mixed_tokens.empty() && std::holds_alternative<int>(mixed_tokens.front())) {
         int current = std::get<int>(mixed_tokens.front());
@@ -78,7 +88,7 @@ void Calculator::calculate(std::string math_operator) {
             result_int /= current;
         }
     }
-
+    calculate(m_op);
     return_result();
 }
 
